9fa76c8fb88d19ed09bd1d2c6f55e6f16139c56a
Tatiana <tatiana.grigorzh@gmail.com>
2025-03-09 12:31:30 +0100

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Tatiana Grigorjevschi',
+    author_email='tatiana.grigorzh@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
